aa4b766117d3cabc7e6caddc1d0bbdbfb7eea038
fanansheng <fanansheng@xt-school.com>
2021-12-06 22:22:27 +0800

    feat: test

--- project-one/.eslintrc.js
+++ project-one/.eslintrc.js
@@ -1,14 +1,37 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  parserOptions: {
-    parser: "babel-eslint",
-  },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-  },
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:vue/essential"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaVersion": 2018,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "vue"
+    ],
+    "rules": {
+      // 禁止对象字面量中出现重复的 key
+    "no-dupe-keys": "error",
+    // 禁止出现重复的 case 标签
+    "no-duplicate-case": "error",
+    // 禁止出现空语句块,允许catch出现空语句
+    "no-empty": ["error", {"allowEmptyCatch": true}],
+    // 禁止对 catch 子句的参数重新赋值
+    "no-ex-assign":"error",
+    // 禁止不必要的布尔转换
+    "no-extra-boolean-cast": "error",
+    // 禁止不必要的分号
+    "no-extra-semi": "error",
+    // 强制所有控制语句使用一致的括号风格
+    "curly": "error"
+    }
 };
