7388d4d8d90827ce516249aa2ee3a08ccf72eec8
fanansheng <fanansheng@xt-school.com>
2021-12-06 23:39:38 +0800

    1

--- project-one/index.js
+++
@@ -1,7 +0,0 @@
-console.log(`hello husky`);
-function test(a){
-                console.log(`number is `,1);
-                
-}
-
-   test(12)
---
+++ project-one/src/index.js
@@ -0,0 +1,6 @@
+console.log(`hello husky`)
+function test(a) {
+	console.log(`number is `, a)
+}
+
+test(12)
