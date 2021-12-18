95e8b53392d867b10d6d7dd825f1c825e0d77fe3
fanansheng <fanansheng@xt-school.com>
2021-12-18 21:43:00 +0800

    feat：合并临时缓存

--- project-one/src/index.js
+++ project-one/src/index.js
@@ -2,5 +2,5 @@ console.log(`hello husky`)
 function test(a) {
 	console.log(`number is `, a)
 }
-const id = '98b1cbc6dd87137d37b8ad4380ee1515c3bb3e3d'
+const id = '520ba089578eee901a08e4e8f5fcfff5cbe76060'
 test(456)
